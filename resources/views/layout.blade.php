b68210e78b9ac9955a87b4c322b0f004595eb5a0
MasudBinMazid <mamun15-5451@diu.edu.bd>
2025-06-26 16:36:21 +0600

    designed layout and project page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,26 +8,40 @@
 </head>
 <body>
   <header>
-    <nav class="navbar">
-      <ul>
-        <li><a href="{{ url('/') }}">Home</a></li>
-        <li><a href="{{ url('/about') }}">About</a></li>
-        <li><a href="{{ url('/blog') }}">Blog</a></li>
-        <li><a href="{{ url('/subscribe') }}">Subscribe</a></li>
-        <li><a href="{{ url('/projects') }}">Projects</a></li>
-        <li><a href="{{ url('/contact') }}">Contact</a></li>
-      </ul>
-    </nav>
-  </header>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <a href="{{ url('/') }}">Masud<span>Portfolio</span></a>
+    </div>
+    <div class="navbar-toggle" id="menu-toggle">&#9776;</div>
+    <ul class="navbar-links" id="nav-links">
+      <li><a href="{{ url('/') }}">Home</a></li>
+      <li><a href="{{ url('/about') }}">About</a></li>
+      <li><a href="{{ url('/blog') }}">Blog</a></li>
+      <li><a href="{{ url('/subscribe') }}">Subscribe</a></li>
+      <li><a href="{{ url('/projects') }}">Projects</a></li>
+      <li><a href="{{ url('/contact') }}">Contact</a></li>
+    </ul>
+  </nav>
+</header>
 
   <main>
     @yield('content')
   </main>
 
-  <footer>
+<footer class="footer">
+  <div class="footer-container">
+    <div class="footer-brand">
+      <h3>Masud<span>Portfolio</span></h3>
+      <p>Data-driven insights, clean design, and powerful storytelling.</p>
+    </div>
+
+  <div class="footer-bottom">
     <p>&copy; 2025 Masud. All rights reserved.</p>
-  </footer>
+  </div>
+</footer>
+
 
   <script src="{{ asset('assets/js/script.js') }}"></script>
 </body>
 </html>
+
