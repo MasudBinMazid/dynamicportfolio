340e34d4377be0243e1be10b65588efd41866322
masudbinmazid <mamun15-5451@diu.edu.bd>
2025-08-28 23:19:34 +0600

    some unnecessary page deleted

--- resources/views/subscribe.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout')
-@section('title', 'Subscribe')
-@section('content')
-<h2 class="s-12">Subscribe</h2>
-<form>
-  <input type="email" placeholder="Enter your email" required>
-  <button class="c-btn" type="submit">Subscribe</button>
-</form>
-@endsection
