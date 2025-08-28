fd1c5766fafbe7a64c7a1486ab4741cd998f7888
masudbinmazid <mamun15-5451@diu.edu.bd>
2025-08-29 04:38:54 +0600

    contact page fixed

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -15,6 +15,20 @@
       <div class="shape shape-3"></div>
       <div class="shape shape-4"></div>
     </div>
+    
+    <!-- Animated Bubbles -->
+    <div class="bubbles-container">
+      <div class="bubble bubble-1"></div>
+      <div class="bubble bubble-2"></div>
+      <div class="bubble bubble-3"></div>
+      <div class="bubble bubble-4"></div>
+      <div class="bubble bubble-5"></div>
+      <div class="bubble bubble-6"></div>
+      <div class="bubble bubble-7"></div>
+      <div class="bubble bubble-8"></div>
+      <div class="bubble bubble-9"></div>
+      <div class="bubble bubble-10"></div>
+    </div>
   </div>
 
   <!-- Contact Header -->
