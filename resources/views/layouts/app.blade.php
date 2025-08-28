ee48d0000c84223dcf2c5800d09d700515e6bbd0
masudbinmazid <mamun15-5451@diu.edu.bd>
2025-08-29 05:00:12 +0600

    favicon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,13 @@
   <meta charset="utf-8">
   <title>@yield('title','Masud Portfolio')</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+  
   <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
   @stack('styles')
 </head>
